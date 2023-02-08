b3dfc874eed0c9cd120ad326362d2fcfac283908
mohammadAliMkh <ma.mohammadkhani1373@gmail.com>
2023-02-08 12:08:17 +0000

    add predict_from_wild method and change some parameters in show_random_predict_images

--- food_vision_project/predict.py
+++ food_vision_project/predict.py
@@ -2,9 +2,14 @@
 import matplotlib.pyplot as plt
 import torch
 import random
+from PIL import Image
+import requests
+from io import BytesIO
+from torchvision import transforms
 
 def show_random_predict_images(model:torch.nn.Module,
                    data:torch.utils.data.DataLoader,
+                   class_names:list,
                    number_rows:int = 3,
                    number_columns:int = 3,
                    figsize:tuple = (10 , 7)):
@@ -14,6 +19,7 @@ def show_random_predict_images(model:torch.nn.Module,
       args:
           model: torch.nn.Module,
           data: torch.utils.data.DataLoader
+          class_names: list of the all class names
           number_rows: number of rows (default = 3, integer)
           number_columns: number of columns (default = 3 , integer)
           figsize: tuple (default = (10 , 7))
@@ -42,9 +48,34 @@ def show_random_predict_images(model:torch.nn.Module,
   for i in range(number_rows * number_columns):
     plt.subplot(number_rows , number_columns , i + 1)
     plt.imshow(img[i].permute(1 , 2 , 0))
-    out = class_names[torch.argmax(model(torch.unsqueeze(img[i] , dim = 0)) , dim = 1)]  
+    predict = class_names[torch.argmax(model(torch.unsqueeze(img[i] , dim = 0)) , dim = 1)]  
     plt.axis(False)
-    if (out == label[i]):
-      plt.title(out , c = "green")
+    if (predict == label[i]):
+      plt.title(predict , c = "green")
     else:
-      plt.title(out , c = "red")
+      plt.title(predict , c = "red")
+
+
+def predict_image_from_wild(image_url:str, model:torch.nn.Module, transformer:transforms, class_names:list):
+  '''
+    Get an Image from Wild and Make it to Tensor and Predict
+
+      args:
+          image_url: url string of the image
+          model: torch.nn.Module
+          transfomer: torchvision.transforms to reshape and make to tensor
+          class_names: list of the all class names
+      
+      returns:
+          plot the image and show predicted label on top of that
+  '''
+  response = requests.get(image_url)
+  img = Image.open(BytesIO(response.content))
+
+  img_tensor = transformer(img)
+  predict = class_names[torch.argmax(model(torch.unsqueeze(img_tensor , dim = 0)) , dim = 1)]
+
+  plt.figure(figsize = (10 , 7))
+  plt.imshow(img_tensor.permute(1 , 2 , 0))
+  plt.axis(False)
+  plt.title(predict)
