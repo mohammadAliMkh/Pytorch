f71b6289c29769f0f9b89c2d4ef6c17c67e779df
mohammadAliMkh <ma.mohammadkhani1373@gmail.com>
2023-03-19 09:19:02 +0000

    change trainable parameters and last layer of the model and then train

--- transfer_learning_for_food_vision/transfer_learning.py
+++ transfer_learning_for_food_vision/transfer_learning.py
@@ -2,10 +2,10 @@ import torchvision
 import torch
 import matplotlib.pyplot as plt
 import os
+import numpy as np
 import shutil
 import torchinfo
-
-import data_setup
+import data_setup, engine, fetch_data, model, predict, train
 
 from pathlib import Path
 from torchvision import transforms
@@ -31,3 +31,40 @@ torchinfo.summary(efficient_model,
                   input_size = (1 , 3  ,224 , 224),
                   col_names = ["input_size" , "output_size", "num_params" , "trainable"],
                   col_width = 20 )
+
+#change the model trainable parameters to False and just keep last layers to change
+for param in efficient_model.features.parameters():
+  param.requires_grad = False
+
+torchinfo.summary(efficient_model,
+                  input_size = (1 , 3  ,224 , 224),
+                  col_names = ["input_size" , "output_size", "num_params" , "trainable"],
+                  col_width = 20 ) #check the trainable parameters again
+
+#change last layer of the model from 1000 to 3 category
+classifier = torch.nn.Sequential(
+    torch.nn.Dropout(p = 0.2 , inplace = True),
+    torch.nn.Linear(in_features = 1280 , out_features = 3 , bias = True)
+)
+
+efficient_model.classifier = classifier
+
+torchinfo.summary(efficient_model,
+                  input_size = (1 , 3  ,224 , 224),
+                  col_names = ["input_size" , "output_size", "num_params" , "trainable"],
+                  col_width = 20 ) #check final layer again
+
+#lets create loss and optimzer methods and set device agnostic code for training
+loss_fn = torch.nn.CrossEntropyLoss()
+optimizer = torch.optim.Adam(params = efficient_model.parameters() , lr = 0.001)
+device = "cuda" if torch.cuda.is_available() else "cpu"
+
+#train
+results1 = train.train(efficient_model, epochs = 10 , train_data = train_dataLoader ,
+            test_data = test_dataLoader , loss_fn = loss_fn ,
+            optimizer = optimizer , device = device)
+
+print("Train Loss:" , np.mean(results1["train_loss"]))
+print("Train Accurac:" , np.mean(results1["train_accuracy"]))
+print("Test Loss:" , np.mean(results1["test_loss"]))
+print("Test Accuracy:" ,np.mean(results1["test_accuracy"]))
