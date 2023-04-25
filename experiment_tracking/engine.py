8a3410b504ff724027b9103bad1c82de5df0a1e6
mohammadAliMkh <ma.mohammadkhani1373@gmail.com>
2023-04-25 13:02:36 +0000

    copy files to experiment_tracking file

---
+++ experiment_tracking/engine.py
@@ -0,0 +1,94 @@
+
+import torch
+device = "cuda" if torch.cuda.is_available() else "cpu"
+
+def train_model(model:torch.nn.Module,
+                data:torch.utils.data.DataLoader,
+                loss_fn:torch.nn.Module,
+                optimizer:torch.optim.Optimizer,
+                device = device):
+  '''
+    Train Model on Each Epoch and Return Train Accuracy and Loss
+
+    args: 
+        model: torch.nn.Module
+        data: torch.utils.data.DataLoader (train data)
+        loss_fn: torch.nn.Module
+        optimizer: torch.optim
+        device: device agnostic parameter (cpu or cuda)
+
+    returns:
+        acc: List of accuracies in each batch trained
+        loss: List of losses in each batch traind
+  '''
+  
+  accuracy = []
+  loss = []
+
+  model.train()
+
+  for batch , (X , y) in enumerate(data):
+
+    X , y = X.to(device) , y.to(device)
+
+    train_logits = model(X)
+
+    train_loss = loss_fn(train_logits , y)
+    loss.append(train_loss.item())
+
+    optimizer.zero_grad()
+
+    train_loss.backward()
+
+    optimizer.step()
+
+    train_predicts = torch.argmax(torch.softmax(train_logits , dim = 1) , dim = 1)
+    train_accuracy = sum(train_predicts == y for train_predicts , y in zip(train_predicts , y))/len(y)
+    accuracy.append(train_accuracy)
+  
+  acc = sum(accuracy)/len(data)
+  loss = sum(loss)/len(data)
+
+  return acc.item()*100 , loss
+
+
+def test_model(model:torch.nn.Module,
+               data:torch.utils.data.DataLoader,
+               loss_fn:torch.nn.Module,
+               device = device):
+  '''
+    Test Model on Each Batch and Return Test Accuracy/Loss
+
+    args:
+        model: torch.nn.Module
+        data: torch.utils.data.DataLoader (test data)
+        loss_fn: torch.nn.Module
+        device: device agnostic parameter (cpu or cuda)
+
+    returns:
+        acc: List of test accuracies in each test batch
+        loss: List of test losses in each test batch
+  '''
+  
+  accuracy = []
+  loss = []
+
+  model.eval()
+
+  with torch.inference_mode():
+    for batch , (X , y) in enumerate(data):
+      X , y = X.to(device) , y.to(device)
+
+      test_logits = model(X)
+
+      test_loss = loss_fn(test_logits , y)
+      loss.append(test_loss)
+
+      test_predicts = torch.argmax(torch.softmax(test_logits , dim = 1) , dim = 1)
+      test_accuracy = sum(test_predicts == y for test_predicts , y in zip(test_predicts , y)) / len(y)
+      accuracy.append(test_accuracy)
+    
+    acc = sum(accuracy) / len(data)
+    loss = sum(loss) / len(data)
+
+    return acc.item() * 100 , loss.item()
---
+++ experiment_tracking/train.py
@@ -0,0 +1,66 @@
+import torch
+from tqdm.auto import tqdm
+from timeit import default_timer as timer
+from torch.utils.data import DataLoader
+from engine import train_model , test_model
+
+device = "cuda" if torch.cuda.is_available() else "cpu"
+
+def train(model:torch.nn.Module,
+          epochs:int,
+          train_data:torch.utils.data.DataLoader,
+          test_data:torch.utils.data.DataLoader,
+          loss_fn:torch.nn.Module,
+          optimizer:torch.optim.Optimizer,
+          device = device):
+  '''
+    Train Model with evaluation in Epoch Times you select
+
+    args:
+        model: torch.nn.Module
+        epochs: number of epochs you want to train your model
+        train_data: torch.utils.data.DataLoader (train data)
+        test_data: torch.utils.data.DataLoader (test data)
+        loss_fn: torch.nn.Module
+        optimizer: torch.optim.Optimizer
+        device: device agnostic parameter (cuda or cpu)
+    
+    returns:
+        result_values: dictionary of the all test and train values (loss and accuray)
+  '''
+
+  
+  result_values = {"test_loss":[],
+                   "train_loss":[],
+                   "test_accuracy":[],
+                   "train_accuracy":[]}
+
+  test_loss_values = []
+  test_accuracy_values = []
+  train_loss_values = []
+  train_accuracy_values = []
+
+  start_time = timer()
+
+  for epoch in tqdm(range(epochs)):
+    train_acc , train_loss = train_model(model , train_data , loss_fn , optimizer , device)
+
+    test_acc , test_loss = test_model(model , test_data , loss_fn , device)
+
+    test_loss_values.append(test_loss)
+    test_accuracy_values.append(test_acc)
+    train_loss_values.append(train_loss)
+    train_accuracy_values.append(train_acc)
+
+    print(f"Epoch {epoch} | Train Loss: {train_loss:0.4f} | Train Accuracy: {train_acc:0.2f} | Test Loss: {test_loss:0.4f} | Test Accuracy: {test_acc:0.2f} ")
+
+  end_time = timer()
+  process_time = end_time - start_time
+  print(f"\nProcess Time: {process_time:0.2f} seconds")
+
+  result_values["test_loss"] = test_loss_values
+  result_values["test_accuracy"] = test_accuracy_values
+  result_values["train_loss"] = train_loss_values
+  result_values["train_accuracy"] = train_accuracy_values
+
+  return result_values
