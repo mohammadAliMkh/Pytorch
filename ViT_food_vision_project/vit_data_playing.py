98c6f4b56f39b5ee4d9706f0abe67835a69662fa
mohammadAliMkh <ma.mohammadkhani1373@gmail.com>
2023-06-20 19:22:29 +0000

    improve plot_patch_image function

--- ViT_food_vision_project/vit_data_playing.py
+++ ViT_food_vision_project/vit_data_playing.py
@@ -1,89 +1,92 @@
-import torch
-import torchvision
-import scipy
-import numpy
-import sklearn
-import pandas
-import matplotlib.pyplot as plt
-import os
-import timeit
-import random
+# import torch
+# import torchvision
+# import scipy
+# import numpy
+# import sklearn
+# import pandas
+# import matplotlib.pyplot as plt
+# import os
+# import timeit
+# import random
 
 
-from torch.utils.data import DataLoader
-from torchvision import datasets
-from torchvision import transforms
+# from torch.utils.data import DataLoader
+# from torchvision import datasets
+# from torchvision import transforms
 
-#load data
-test_data_path = "/content/Pytorch/ViT_food_vision_project/pizza_steak_sushi/test"
-train_data_path = "/content/Pytorch/ViT_food_vision_project/pizza_steak_sushi/train"
+# #load data
+# test_data_path = "/content/Pytorch/ViT_food_vision_project/pizza_steak_sushi/test"
+# train_data_path = "/content/Pytorch/ViT_food_vision_project/pizza_steak_sushi/train"
 
-#set image size and make transformer
-IM_SIZE = 224
-BATCH_SIZE = 32
+# #set image size and make transformer
+# IM_SIZE = 224
+# BATCH_SIZE = 32
 
-transformer = transforms.Compose([
-    transforms.Resize((IM_SIZE , IM_SIZE)),
-    transforms.ToTensor()
-])
+# transformer = transforms.Compose([
+#     transforms.Resize((IM_SIZE , IM_SIZE)),
+#     transforms.ToTensor()
+# ])
 
-#create test and train dataLoaders
-test_dataset = datasets.ImageFolder(test_data_path , transform = transformer)
-train_dataset = datasets.ImageFolder(train_data_path , transform = transformer)
+# #create test and train dataLoaders
+# test_dataset = datasets.ImageFolder(test_data_path , transform = transformer)
+# train_dataset = datasets.ImageFolder(train_data_path , transform = transformer)
 
-class_names = train_dataset.classes
+# class_names = train_dataset.classes
 
-test_dataLoader = DataLoader(test_dataset, batch_size = BATCH_SIZE , shuffle = False , num_workers = torch.cuda.device_count())
-train_dataLoader = DataLoader(train_dataset, batch_size = BATCH_SIZE , shuffle = True , num_workers = torch.cuda.device_count())
+# test_dataLoader = DataLoader(test_dataset, batch_size = BATCH_SIZE , shuffle = False , num_workers = torch.cuda.device_count())
+# train_dataLoader = DataLoader(train_dataset, batch_size = BATCH_SIZE , shuffle = True , num_workers = torch.cuda.device_count())
 
-#show random images of data
-columns = 3
-rows = 4
+# #show random images of data
+# columns = 3
+# rows = 4
 
-data = next(iter(train_dataLoader))
+# data = next(iter(train_dataLoader))
 
-rand = random.randint(0 , len(data[0]) - (columns * rows))
+# rand = random.randint(0 , len(data[0]) - (columns * rows))
 
-images = data[0][rand:rand + (columns * rows)]
-targets = data[1][rand:rand + (columns * rows)]
+# images = data[0][rand:rand + (columns * rows)]
+# targets = data[1][rand:rand + (columns * rows)]
 
-plt.figure(figsize = (10 , 7))
-for i in range(0 , (columns * rows)):
-  plt.subplot(columns , rows , i + 1)
-  plt.imshow(torch.permute(images[i] , dims = (1 , 2 , 0)))
-  plt.title(class_names[targets[i]])
-  plt.axis(False)
-
-
-#create a function to plot an image after empatching it
-def plot_patched_image(image , patch_size:int):
-  ''' 
-    plot an image after cropping image into patchs
-      
-      inputs:
-        image: an torch permutetd image in shape (H , W , C)
-        patch_size : an integere odd number
+# plt.figure(figsize = (10 , 7))
+# for i in range(0 , (columns * rows)):
+#   plt.subplot(columns , rows , i + 1)
+#   plt.imshow(torch.permute(images[i] , dims = (1 , 2 , 0)))
+#   plt.title(class_names[targets[i]])
+#   plt.axis(False)
 
 
-      output:
-        plot a figure with patched sub images
-  '''
+# #create a function to plot an image after empatching it
+# def plot_patched_image(image , patch_size:int):
+#   '''
+#     plot an image after cropping image into patchs
 
-  patch_size = patch_size
-  sample_image = image
-  number_of_patches = int(sample_image.shape[0] / patch_size)
-  n_rows = number_of_patches
-  n_columns = n_rows
-  n_fig = 1
+#       inputs:
+#         image: an torch permutetd image in shape (H , W , C)
+#         patch_size : an integere odd number
 
-  plt.figure(figsize = (patch_size , patch_size))
-  for i in range(n_rows):
-    for j in range(n_columns):
-      plt.subplot(n_rows , n_columns , n_fig)
-      plt.imshow(sample_image[i * patch_size: (i + 1) * patch_size , j * patch_size: (j + 1) * patch_size , :])
-      plt.axis(False)
-      n_fig = n_fig + 1
 
-#show the result
-image = torch.permute(images[0] , (1 , 2  , 0))
-plot_patched_image(image , patch_size= 16)
+#       output:
+#         plot a figure with patched sub images
+#   '''
+
+#   patch_size = patch_size
+#   sample_image = image
+#   number_of_patches = int(sample_image.shape[0] / patch_size)
+#   n_rows = number_of_patches
+#   n_columns = n_rows
+
+#   fig , axs = plt.subplots(n_rows , n_columns , sharex = True , sharey = True , figsize = (10 , 10))
+
+#   for i in range(n_rows):
+#     for j in range(n_columns):
+#       axs[i , j].imshow(sample_image[i * patch_size: (i + 1) * patch_size , j * patch_size: (j + 1) * patch_size , :])
+#       axs[i , j].set_xlabel(j+1)
+#       axs[i , j].set_ylabel(i+1 , rotation  = "horizontal" , ha = "right")
+#       axs[i , j].set_xticks([])
+#       axs[i , j].set_yticks([])
+#       axs[i , j].label_outer()
+#   plt.show()
+
+# #show the result
+# image = torch.permute(images[0] , (1 , 2  , 0))
+# plot_patched_image(image , patch_size= 16)
