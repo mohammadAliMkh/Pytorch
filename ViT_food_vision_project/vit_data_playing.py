6453e652ed00325c4a9eaaeda47e75c37dd188ce
mohammadAliMkh <ma.mohammadkhani1373@gmail.com>
2023-06-21 19:36:34 +0000

    create patch class to create ViT input for Transfomer and Test it

--- ViT_food_vision_project/vit_data_playing.py
+++ ViT_food_vision_project/vit_data_playing.py
@@ -107,7 +107,7 @@ def prepare_embed_input(image):
 
 
 
-plot some embedded visually
+#plot some embedded visually
 fig , axs = plt.subplots(3 , 3 , figsize = (5 , 5 ), sharex = True , sharey = True)
 n_fig = 0
 
@@ -123,26 +123,47 @@ for i in range( 3):
 
 
 
-create Patching class befor adding class token and position embeddings
-class Patching(torch.nn.Module):
+#create Patching class to create our transformer input
+class PatchEmbedding(torch.nn.Module):
 
 
-  def __init__(self , in_channels = 3,
-               out_channels = 768,
-               kernel_size = 16,
-               stride = 16,
-               padding = 0):
-    
+  def __init__(self ,in_heights = 224 , in_widths = 224 ,  in_channels = 3, patch_size = 16 , batch_size = 1):
+
     super().__init__();
+
+    self.class_token = torch.nn.Parameter(
+        torch.rand(batch_size , 1 , patch_size**2 * in_channels),
+        requires_grad = True
+        )
+
+    self.position_embeds = torch.nn.Parameter(
+        torch.rand((batch_size, int(in_heights * in_widths / patch_size**2) + 1, patch_size**2 * in_channels)),
+        requires_grad = True)
+
     self.patches = torch.nn.Conv2d(in_channels = in_channels,
-                                   out_channels = out_channels,
-                                   kernel_size = kernel_size,
-                                   stride = stride,
-                                   padding = padding)
-    
+                                   out_channels = patch_size * patch_size * in_channels,
+                                   kernel_size = patch_size,
+                                   stride = patch_size,
+                                   padding = 0)
+
     self.flat_patches = torch.nn.Flatten(start_dim = 2 , end_dim = 3)
 
 
-
   def forward(self , x):
-    return torch.permute(self.flat_patches(self.patches(x)) , (0 , 2 , 1))
+
+    patches = self.patches(x)
+
+    flat_patches = self.flat_patches(patches)
+    #print(flat_patches.shape)
+    flat_patches = torch.permute(flat_patches , (0 , 2 , 1))
+    #print(flat_patches.shape)
+    flat_patches_with_class_token = torch.cat((flat_patches , self.class_token) , dim = 1)
+    #print(flat_patches_with_class_token.shape)
+    flat_patches_with_class_token_and_position_embeds = flat_patches_with_class_token + self.position_embeds
+    #print(flat_patches_with_class_token_and_position_embeds.shape)
+    return flat_patches_with_class_token_and_position_embeds
+
+# test ou class and our first part of the ViT model
+patchify = PatchEmbedding(patch_size = 16)
+transformer_input = patchify(torch.unsqueeze(torch.permute(image , (2 , 0 , 1)) , dim = 0))
+transformer_input.shape
